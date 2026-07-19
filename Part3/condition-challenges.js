e654ba2d78b3e1cebf9c2d7bb01f7e2df74493ce
Suham Pandey <pandeysuham280@gmail.com>
2026-07-19 20:59:29 +0545

    Conditional

---
+++ Part3/condition-challenges.js
@@ -0,0 +1,61 @@
+// Check if a number is greater than another number
+
+let num1 = 5;
+let num2 = 8;
+// console.log("I am regular upper code");
+
+// if (num1 > num2) {
+//     console.log("num 1 is greater than num 2");
+// } else{
+//     console.log("Nope, num 1 is not greater than num 2");
+// }
+
+// console.log("I am regular bottom code");
+
+// Checking if a string is equal to another string:
+
+let username = "Suham";
+let anotherUsername = "Suham";
+
+// if (username == anotherUsername){
+//     console.log("Pick another username");    
+// }
+
+// if (username == anotherUsername){
+//     console.log("Pick another username");    
+// } else{
+//     console.log("you can pick this username");    
+// }
+
+// Cheking if a variable is a number or not:
+
+let score = 44;
+
+if (typeof score === 'number') {
+    console.log("yep, this is a number");    
+} else {
+    console.log("No that is not a number");
+}
+
+// Check if a boolean value if true or not:
+
+let isTeaReady = false;
+
+if(isTeaReady){
+    console.log("Tea is Ready");
+} else {
+    console.log("Tea is not Ready");  
+}
+
+// Check if an array is empty or not:
+
+let items = [];
+console.log(items.length);
+
+if (items.length === 0) {
+    console.log("Array is empty");
+    
+} else {
+    console.log("Array is NOT empty");
+    
+}
